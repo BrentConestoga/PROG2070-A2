f774ab71d71a629fd2ddaf1a0776a1d97e548055
Brent Baillie <brentnathanbaillie729@gmail.com>
2025-02-05 16:39:04 -0500

    First commi

---
+++ Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070_A2_Group_10
+{
+	internal class Product
+	{
+	}
+}
