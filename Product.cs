4b96823984b54ed11596e63b5ec203c0f09e3f33
Brent Baillie <brentnathanbaillie729@gmail.com>
2025-02-05 16:55:06 -0500

    Product class finalized.

--- Product.cs
+++ Product.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace PROG2070_A2_Group_10
 {
-	internal class Product
+	public class Product
 	{
+		public int ProdID { get; set; }
+		public string ProdName { get; set; }
+		public float ItemPrice { get; set; }
+		public int StockAmount { get; set; }
+
+		public Product(int ProdID, string ProdName, float ItemPrice, int StockAmount)
+		{
+			this.ProdID = ProdID;
+			this.ProdName = ProdName;
+			this.ItemPrice = ItemPrice;
+			this.StockAmount = StockAmount;
+		}
+
+		public void IncreaseStock(int amount)
+		{
+			if (amount < 0)
+			{
+				return;
+			}
+			StockAmount += amount;
+		}
+
+		public void DecreaseStock(int amount)
+		{
+			if (StockAmount == 0 || amount < 0)
+			{
+				return;
+			}
+			StockAmount -= amount;
+		}
 	}
 }
